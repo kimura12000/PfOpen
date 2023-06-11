2b611918070080c963bd2882b2c671e277840f17
kimura12000 <75467486+kimura12000@users.noreply.github.com>
2023-06-12 00:08:52 +0900

    Update Profile.jsx

--- pages/Profile.jsx
+++ pages/Profile.jsx
@@ -8,11 +8,11 @@ export default function Profile () {
       <div>
       <h3 className={styles.wdTitle} style={{paddingTop: '160px',paddingLeft: '20px',textAlign: 'left', fontSize:'24px'}}>PROFILE</h3>
       <p className={styles.prfDescript}>韓国財閥企業の日本法人にてグループ会社含め全６社分のWEBサイトの新BI改定に伴うリニューアル/新サイトの立ち上げ/LP制作/管理更新改定を一人で担当しました(2022年)。<br/>
-      その間、中国語オンラインスクールサービスサイトのデザイン・Vue.js/Laravelで構築されたUI/UXのフロントエンド全般をデザイン。<br/>
+      その間、中国語オンラインスクールサービスサイトのWEBデザイン及び・Vue.js/Nuxt/Laravelで構築されたUI/UXのフロントエンド全般を担当。<br/>
 東証１部上場の外資系人材紹介会社にて 転職情報やコラムの入稿・パーツ制作・経歴書ページ制作・Wordpressへのコンバージョン業務を担当しました。(2021年)<br/>
 2020年よりテレビ局系列会社にて、フリップ・ニュース番組やバラエティ番組のモニターデザイン制作を担当しました。<br/>
 2017年に安田不動産の複合施設ワテラスのイルミネーションデザインを担当。<br/>
-2014年〜2020年まで呉服店にてDMやPOPのデザイン・WEBデザイン・業界組合のWEBデザインを担当しました。<br/><br/><br/>
+2014年〜2018年まで呉服店にてDMやPOPのデザイン・WEBデザイン・業界組合のWEBデザインを担当しました。<br/><br/><br/>
 使用ソフト：Adobe Illustrator /Photoshop /Figma /XD /Blender /AfterEffects<br/>
 Web制作環境：HTML CSS Javascript React Next.js Jquery PHP Wordpress Node.js Three.js GSAP Bootstrap など<br/>
 勉強中言語:Javascript Three.js Typescript React.js Redux Next.js ChakraUI Node.js PHP Laravel</p>
